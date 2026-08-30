7600c88b659f2f98ae2ce6acea08a651ea04fb66
beiwei91 <g20060203@126.com>
2026-08-30 22:45:16 +0800

    Changes

---
+++ FrontEnd/src/views/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="head">
+    <text>登录</text>
+  </div>
+
+  <div class="body">
+    <div>
+      <text>账户：</text>
+      <input type="text">
+    </div>
+
+    <div>
+      <text>密码：</text>
+      <input type="password">
+    </div>
+
+    <div>
+      <button>注册</button>
+      <button>登录</button>
+    </div>
+  </div>
+
+  <div class="rare">
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
